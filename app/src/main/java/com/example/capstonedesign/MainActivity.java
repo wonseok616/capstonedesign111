127781356f9e6095b62dbdc208641fefe0ac3e4a
wonseok616 <wonseok000616@gmail.com>
2021-05-10 20:09:29 +0900

    Third Commit

---
+++ app/src/main/java/com/example/capstonedesign/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.capstonedesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public class SpinnerActivity extends Activity implements AdapterView.OnItemSelectedListener{
+
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
+    }
+}
